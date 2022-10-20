c585d115d207d2f7641db5e6d74a3ffe04078338
shivshankar123ch <89379054+shivshankar123ch@users.noreply.github.com>
2022-10-19 23:58:46 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# automatefile
+# FileManagerScript
+pip install wheel
+python setup.py sdist bdist_wheel
+
+<!-- packages -->
+automate-0.1-py3-none-any.whl
+automate-0.1.tar.gz
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='automatefile',
+    version=0.9,
+    descriptions='automation of file bet package',
+    author='S.S. Chaudhary MIT',
+    packages=['automatefile'],
+    package_dir={'automatefile': 'automatefile'},
+    install_requires=['Admin', 'six'],
+    url ='https://github.com/shivshankar123ch/automatefile.git'
+)
